a7f59dbbf2ee451237aaa268d15f0242d0761b18
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2022-11-21 04:25:28 +0900

    Add test to accept escrow

--- escrow_contracts/tests/harness.rs
+++ escrow_contracts/tests/harness.rs
@@ -1,6 +1,6 @@
 use fuels::prelude::*;
 use fuels::signers::wallet::WalletUnlocked;
-use fuels::tx::ContractId;
+use fuels::tx::{AssetId, ContractId};
 use fuels_abigen_macro::abigen;
 use rand::prelude::{Rng, SeedableRng, StdRng};
 
@@ -113,8 +113,8 @@ async fn setup_tests() -> (
 async fn can_initialize_escrow() {
     let (_, creator, receiver, _, creator_asset_id, receiver_asset_id) = setup_tests().await;
 
-    let creator_asset_amount = 1_000;
-    let requested_amount = 1000;
+    let creator_asset_amount = 100;
+    let requested_amount = 100;
 
     let tx_params: TxParameters = TxParameters::new(None, Some(1_000_000), None);
 
@@ -141,4 +141,181 @@ async fn can_initialize_escrow() {
         .unwrap();
 
     assert_eq!(result.value, 0);
+}
+
+#[tokio::test]
+async fn can_accept_escrow() {
+    let (_, creator, receiver, _, creator_asset_id, receiver_asset_id) = setup_tests().await;
+
+    let creator_asset_amount = 100;
+    let requested_amount = 100;
+
+    let creator_asset_id = Some(AssetId::from(*creator_asset_id)).unwrap();
+    let receiver_asset_id: AssetId = Some(AssetId::from(*receiver_asset_id)).unwrap();
+
+    // Get asset balances
+    let creator_initial_creator_balance = creator
+        .wallet
+        .get_asset_balance(&creator_asset_id)
+        .await
+        .unwrap();
+    let creator_initial_receiver_balance = creator
+        .wallet
+        .get_asset_balance(&receiver_asset_id)
+        .await
+        .unwrap();
+
+    let receiver_initial_creator_balance = receiver
+        .wallet
+        .get_asset_balance(&creator_asset_id)
+        .await
+        .unwrap();
+    let receiver_initial_receiver_balance = receiver
+        .wallet
+        .get_asset_balance(&receiver_asset_id)
+        .await
+        .unwrap();
+    // end get asset balances
+    assert_eq!(creator_initial_receiver_balance, 0);
+    assert_eq!(receiver_initial_creator_balance, 0);
+
+    // creator creates escrow
+    let create_tx_params: TxParameters = TxParameters::new(None, Some(1_000_000), None);
+    let create_call_params =
+        CallParameters::new(Some(creator_asset_amount), Some(creator_asset_id), None);
+
+    let receiver_contract_id = ContractId::new(receiver_asset_id.into());
+
+    let escrow_id = creator
+        .escrow
+        .methods()
+        .create(
+            Identity::Address(receiver.wallet.address().into()),
+            receiver_contract_id,
+            requested_amount,
+        )
+        .tx_params(create_tx_params)
+        .call_params(create_call_params)
+        .call()
+        .await
+        .unwrap();
+
+    let creator_current_creator_balance = creator
+        .wallet
+        .get_asset_balance(&creator_asset_id)
+        .await
+        .unwrap();
+    let creator_current_receiver_balance = creator
+        .wallet
+        .get_asset_balance(&receiver_asset_id)
+        .await
+        .unwrap();
+
+    let receiver_current_creator_balance = receiver
+        .wallet
+        .get_asset_balance(&creator_asset_id)
+        .await
+        .unwrap();
+    let receiver_current_receiver_balance = receiver
+        .wallet
+        .get_asset_balance(&receiver_asset_id)
+        .await
+        .unwrap();
+
+    // assertions for creating escrow
+    assert_eq!(escrow_id.value, 0);
+
+    assert_eq!(
+        creator_current_creator_balance,
+        creator_initial_creator_balance
+            .checked_sub(creator_asset_amount)
+            .unwrap()
+    );
+    assert_eq!(
+        creator_current_receiver_balance,
+        creator_initial_receiver_balance
+    );
+    assert_eq!(
+        receiver_current_receiver_balance,
+        receiver_initial_receiver_balance
+    );
+    assert_eq!(
+        receiver_current_creator_balance,
+        receiver_initial_creator_balance
+    );
+    // end of create escrow with creator
+
+    // receiver accepts escrow
+    let receive_tx_params: TxParameters = TxParameters::new(None, Some(1_000_000), None);
+    let receive_call_params =
+        CallParameters::new(Some(requested_amount), Some(receiver_asset_id), None);
+
+    receiver
+        .escrow
+        .methods()
+        .accept(escrow_id.value)
+        .append_variable_outputs(2)
+        .tx_params(receive_tx_params)
+        .call_params(receive_call_params)
+        .call()
+        .await
+        .unwrap();
+
+    let creator_current_creator_balance = creator
+        .wallet
+        .get_asset_balance(&creator_asset_id)
+        .await
+        .unwrap();
+    let creator_current_receiver_balance = creator
+        .wallet
+        .get_asset_balance(&receiver_asset_id)
+        .await
+        .unwrap();
+
+    let receiver_current_creator_balance = receiver
+        .wallet
+        .get_asset_balance(&creator_asset_id)
+        .await
+        .unwrap();
+    let receiver_current_receiver_balance = receiver
+        .wallet
+        .get_asset_balance(&receiver_asset_id)
+        .await
+        .unwrap();
+    // assertions for accepting escrow
+    assert_eq!(
+        creator_current_creator_balance,
+        creator_initial_creator_balance
+            .checked_sub(creator_asset_amount)
+            .unwrap()
+    );
+    assert_eq!(
+        creator_current_receiver_balance,
+        creator_initial_receiver_balance
+            .checked_add(requested_amount)
+            .unwrap()
+    );
+    assert_eq!(
+        receiver_current_receiver_balance,
+        receiver_initial_receiver_balance
+            .checked_sub(requested_amount)
+            .unwrap()
+    );
+    assert_eq!(
+        receiver_current_creator_balance,
+        receiver_initial_creator_balance
+            .checked_add(creator_asset_amount)
+            .unwrap()
+    );
+    // end of receiver accepts escrow
+
+    // Experiment: another assertion
+    let asset_id: AssetId = AssetId::new(*receiver_contract_id);
+    let creator_requested_funds_bal: Result<u64, Error> =
+        creator.wallet.get_asset_balance(&asset_id).await;
+    let result: u64 = creator_requested_funds_bal.unwrap();
+
+    print!("asset_id {:?}, balance {:?}", asset_id, result);
+
+    assert_eq!(result, requested_amount);
 }
